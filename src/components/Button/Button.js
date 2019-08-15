b6583accdb8628c6837d29842d777a8211ed6464
Audun Rundberg <audun@netliferesearch.com>
2019-08-15 13:56:13 +0200

    add classnames and update button example a little

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,11 +1,22 @@
 import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
 
 import "./Button.css";
 
-const Button = ({ children }) => (
-  <button className="bg-blue-700 hover:bg-regal-blue text-white font-bold py-2 px-4 rounded">
-    {children}
-  </button>
+const buttonClasses = variant =>
+  classNames({
+    "bg-blue-700 hover:bg-regal-blue": !variant || variant === "primary",
+    "bg-gray-700 hover:bg-gray-900": variant === "secondary",
+    "text-white font-bold py-2 px-4 rounded": true
+  });
+
+const Button = ({ variant, children }) => (
+  <button className={buttonClasses(variant)}>{children}</button>
 );
+
+Button.propTypes = {
+  variant: PropTypes.string
+};
 
 export default Button;
--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,12 +1,24 @@
 import React from "react";
 
 import { storiesOf } from "@storybook/react";
-import { text } from "@storybook/addon-knobs";
+import { text, select } from "@storybook/addon-knobs";
 
 import Button from ".";
 
+const variants = {
+  Primary: "primary",
+  Secondary: "secondary",
+  Default: null
+};
+
 storiesOf("Button", module)
-  .add("with text", () => <Button>{text("Text", "I am a button")}</Button>)
-  .add("with other text", () => (
-    <Button>{text("Text", "I am a button too")}</Button>
+  .add("primary", () => (
+    <Button variant={select("Variant", variants, "primary")}>
+      {text("Text", "I am a button")}
+    </Button>
+  ))
+  .add("secondary", () => (
+    <Button variant={select("Variant", variants, "secondary")}>
+      {text("Text", "I am a button too")}
+    </Button>
   ));
