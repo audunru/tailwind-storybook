127aedaf9bda0ca55ae3877dd531ba4bcfbccb18
Audun Rundberg <audun@netliferesearch.com>
2019-08-15 10:05:19 +0200

    remove addon-links

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,5 +1,4 @@
-import "@storybook/addon-actions/register";
-import "@storybook/addon-links/register";
-import "@storybook/addon-a11y/register";
 import "storybook-react-to-static-markup/register";
 import "@storybook/addon-knobs/register";
+import "@storybook/addon-actions/register";
+import "@storybook/addon-a11y/register";
