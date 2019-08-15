c15978aad5f488b4ab7ea1e938280e793fdb9714
Audun Rundberg <audun@netliferesearch.com>
2019-08-15 09:48:59 +0200

    example tailwind.config.js

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,7 +3,7 @@ import React from "react";
 import "./Button.css";
 
 const Button = ({ children }) => (
-  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+  <button className="bg-blue-700 hover:bg-regal-blue text-white font-bold py-2 px-4 rounded">
     {children}
   </button>
 );
