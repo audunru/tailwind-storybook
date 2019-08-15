926be1ba683dc45a2372784a53ec1999117b4eb9
Audun Rundberg <audun@netliferesearch.com>
2019-08-15 14:00:57 +0200

    update card component example

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,13 +3,9 @@ import PropTypes from "prop-types";
 
 import "./Card.css";
 
-const Card = ({ title = "", children }) => (
+const Card = ({ title = "", image = "", children }) => (
   <div className="max-w-sm rounded overflow-hidden shadow-lg">
-    <img
-      className="w-full"
-      src="https://placekitten.com/408/287"
-      alt="Sunset in the mountains"
-    />
+    <img className="w-full" src={image} alt="Sunset in the mountains" />
     <div className="px-6 py-4">
       <div className="font-bold text-xl mb-2">{title}</div>
       <p className="text-gray-700 text-base">{children}</p>
@@ -18,7 +14,8 @@ const Card = ({ title = "", children }) => (
 );
 
 Card.propTypes = {
-  title: PropTypes.string
+  title: PropTypes.string,
+  image: PropTypes.string
 };
 
 export default Card;
--- src/components/Card/Card.stories.js
+++ src/components/Card/Card.stories.js
@@ -6,7 +6,10 @@ import { text } from "@storybook/addon-knobs";
 import Card from ".";
 
 storiesOf("Card", module).add("with title and body", () => (
-  <Card title={text("Title", "I am a card")}>
-    {text("Body", "I have a title and a body")}
+  <Card
+    image={text("Image URL", "https://placekitten.com/408/287")}
+    title={text("Title", "I am a card")}
+  >
+    {text("Body", "I have an image, a title and a body")}
   </Card>
 ));
