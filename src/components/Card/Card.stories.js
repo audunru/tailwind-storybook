5880c2db5e4b4beeb256b43f3069bda672c505fb
Audun Rundberg <audun@netliferesearch.com>
2019-08-15 13:56:24 +0200

    update card story description

--- src/components/Card/Card.stories.js
+++ src/components/Card/Card.stories.js
@@ -5,7 +5,7 @@ import { text } from "@storybook/addon-knobs";
 
 import Card from ".";
 
-storiesOf("Card", module).add("with body", () => (
+storiesOf("Card", module).add("with title and body", () => (
   <Card title={text("Title", "I am a card")}>
     {text("Body", "I have a title and a body")}
   </Card>
