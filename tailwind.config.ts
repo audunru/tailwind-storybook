639665bfbac239eb0f4380aeeefd220defae010a
Audun Rundberg <audun@rundberg.no>
2025-02-02 10:23:08 +0100

    build: tailwind 4 (#20)
    * build: update dependencies
    
    * build: deps
    
    * build: deps
    
    * build: deps
    
    * build: tailwind 4

--- tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "tailwindcss";
-
-export default {
-  content: ["./index.html", "./src/**/*.{ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-} satisfies Config;
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   test: {
     globals: true,
     environment: "jsdom",
