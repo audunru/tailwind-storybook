3671d65f9fb0933ef7d9433768ce1a5517adf211
Audun Rundberg <audun@rundberg.no>
2026-05-16 09:46:53 +0200

    chore: biome (#90)

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+// biome-ignore lint/style/noNonNullAssertion: root element is guaranteed by index.html
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
